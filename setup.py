8c7dd7bd1fca5dedb71a4772237f2a2811f50688
Bastien Pasdeloup <bastien.pasdeloup@gmail.com>
2024-04-24 18:17:30 +0200

    updted dependencies

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ import setuptools
 setuptools.setup \
 (
     name =                          "PyRat",
-    version =                       "5.3.0",
+    version =                       "5.4.0",
     author =                        "Bastien Pasdeloup",
     author_email =                  "bastien.pasdeloup@imt-atlantique.fr",
     description =                   "PyRat softare used in the computer science at IMT Atlantique",
@@ -33,7 +33,7 @@ setuptools.setup \
     license =                       "MIT",
     packages =                      setuptools.find_packages(),
     include_package_data =          True,
-    install_requires =              ["pygame", "colored", "distinctipy", "numpy", "scipy", "tqdm", "typing_extensions", "matplotlib", "pdoc"],
+    install_requires =              ["pygame", "colored", "distinctipy", "numpy", "scipy", "tqdm", "typing_extensions", "matplotlib", "torch"],
 )
 
 #####################################################################################################################################################
